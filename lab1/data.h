a85f3370bdad7a822c1b07d2b85987869cb491b0
solarrm <89846246+solarrm@users.noreply.github.com>
2022-02-23 23:48:41 +0300

    Add files via upload

---
+++ lab1/data.h
@@ -0,0 +1,10 @@
+#pragma once
+
+int x = 10, y = 4;
+int result1, result2, result3, result4;
+float q = 2.5, e = 3.3;
+int& rx = x, & ry = y;
+float& rq = q;
+float* ptre = &e;
+int* ptrx = &x, * ptry = &y;
+int arr[3][3];
---
+++ lab1/include.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <locale>
+
+using namespace std;
