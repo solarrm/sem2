ded4fd7b494421de12070e59b86ba3277c12f175
solarrm <89846246+solarrm@users.noreply.github.com>
2022-02-24 13:18:14 +0300

    Add files via upload

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -3,58 +3,63 @@
 
 int main() {
 	setlocale(LC_ALL, "Ru");
-	
-	
-	cout << "Задание №2" << endl;
-	result1 = rx + ry;
-	cout << result1 << endl;
-	result2 = *ptrx + *ptry;
-	cout << result2 << endl;
-
-	
-	cout << "\n\n" << "Задание №7" << endl;
-	cout << -1 * rx << ", " << -1 * *ptry << endl;
-
-	
-	cout << "\n\n" << "Задание №10" << endl;
-	cout << "Введите переменную X и I: " << endl;
-	cin >> rq >> *ptre;
-	cout << "Вы ввели: z=" << rq;
-	if (*ptre > 0) {
-		cout << "+" << *ptre << "i" << endl;
+	cout << "Введите номер задания..." << endl;
+	int command;
+	cin >> command;
+	if (command == 1) {
+		cout << "Задание №2" << endl;
+		result1 = rx + ry;
+		cout << result1 << endl;
+		result2 = *ptrx + *ptry;
+		cout << result2 << endl;
 	}
-	if (*ptre < 0) {
-		cout << *ptre << "i" << endl;
+	if (command == 2) {
+		cout << "\n\n" << "Задание №7" << endl;
+		cout << -1 * rx << ", " << -1 * *ptry << endl;
 	}
-	cout << "Комплексно сопряженная переменная: z'=" << rq;
-	if (*ptre < 0) {
-		cout << "+" << *ptre * -1 << "i" << endl;
-	}
-	if (*ptre > 0) {
-		cout << *ptre * -1 << "i" << endl;
-	}
-
-	
-	cout << "\n\n" << "Задание №15" << endl << "Введите коэффициенты матрицы: " << endl;
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			cin >> arr[i][j];
+	if (command == 3) {
+		cout << "\n\n" << "Задание №10" << endl;
+		int c = *ptre;
+		int v = rq;
+		cout << "Введите переменную X и I: " << endl;
+		cin >> rq >> *ptre;
+		cout << "Вы ввели: z=" << rq;
+		if (*ptre > 0) {
+			cout << "+" << *ptre << "i" << endl;
+		}
+		if (*ptre < 0) {
+			cout << *ptre << "i" << endl;
+		}
+		cout << "Комплексно сопряженная переменная: z'=" << rq;
+		if (*ptre < 0) {
+			cout << "+" << *ptre * -1 << "i" << endl;
+		}
+		if (*ptre > 0) {
+			cout << *ptre * -1 << "i" << endl;
 		}
 	}
-	cout << endl;
-
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			cout << arr[i][j] * rq << " ";
+	if (command == 4) {
+		cout << "\n\n" << "Задание №15" << endl << "Введите коэффициенты матрицы: " << endl;
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				cin >> arr[i][j];
+			}
 		}
 		cout << endl;
-	}
-	cout << "\n\n";
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 3; ++j) {
-			cout << arr[i][j] * *ptre << " ";
+
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				cout << rq * arr[i][j] << " ";
+			}
+			cout << endl;
 		}
-		cout << endl;
+		cout << "\n\n";
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				cout << arr[i][j] * *ptre << " ";
+			}
+			cout << endl;
+		}
 	}
 	return 0;
 }
